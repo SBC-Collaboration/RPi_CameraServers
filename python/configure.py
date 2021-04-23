41a15efe746a89f4040c1114430fb1590c788c04
Zhiheng Sheng <shengzhiheng@gmail.com>
2021-04-23 11:18:53 -0500

    update capture code and config file

--- python/configure.py
+++ python/configure.py
@@ -4,7 +4,7 @@ config_path = "config.json"
 
 config = {}
 config["adc_threshold"] = 10
-config["exposure"] = 1
+config["exposure"] = 500
 config["pix_threshold"] = 199 #15
 config["max_frames"] = 100
 config["frames_after"] = 50
@@ -13,6 +13,19 @@ config["resolution"] = [1280,800]
 config["save_path"] = "./Captures/"
 config["image_format"] = ".jpg"
 config["date_format"] = "%Y-%m-%d_%H:%M:%S"
+config["input_pins"] = {"state_com": 7,
+                        "trig_en": 9,
+                        "trig_latch": 11}
+config["output_pins"] = {"state": 2,
+                         "trig": 4}
+config["frame_sync"] = False
+config["registers"] = [[0x4F00, 0x01],
+                       [0x3030, 0x04],
+                       [0x303F, 0x01],
+                       [0x302C, 0x00],
+                       [0x302F, 0x7F],
+                       [0x3823, 0x30],
+                       [0x0100, 0x00],]
 
 def save_config(config_path):
     with open(config_path, "w") as f:
--- python/imdaq.py
+++ python/imdaq.py
@@ -12,17 +12,20 @@ import os
 import numpy as np
 from PIL import Image
 import time
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import ctypes as ct
 from count import count_above
 import os
 import json
+from multiprocessing import Process
+import threading
 
 class CaptureCore:
     def __init__(self):
         self.config_path = "config.json"
         self.load_config(self.config_path)
         self.init_camera()
+        self.init_threading()
         
     def load_config(self, config_path):
         print("Loading config")
@@ -36,19 +39,22 @@ class CaptureCore:
         self.camera.init_camera()
         
         print("Camera open")
-        self.camera.set_resolution(*self.resolution)
+        self.camera.set_resolution(*self.config["resolution"])
         self.camera.set_mode(5)
         self.camera.set_control(v4l2.V4L2_CID_VFLIP, 1)
         self.camera.set_control(v4l2.V4L2_CID_HFLIP,1)
-        self.camera.set_control(v4l2.V4L2_CID_EXPOSURE,1)
+        #self.camera.software_auto_exposure(enable = True)
+        self.camera.set_control(v4l2.V4L2_CID_EXPOSURE,self.config["exposure"])
+        if self.config["frame_sync"]:
+            regs = self.config["registers"]
+            for i in range(7):
+                self.camera.write_sensor_reg(regs[i][0],regs[i][1])
         print("Camera set")
 
-    def set_controls(self):
-        try:
-            print("Enable Auto Exposure...")
-            self.camera.software_auto_exposure(enable = True)
-        except Exception as e:
-            print(e)
+    def init_threading(self):
+        start_proc = threading.Event()
+        next_frame = threading.Event()
+        start_cap = threading.Event()
 
     def capture(self):
         frame = self.camera.capture(encoding = self.image_format)
@@ -58,6 +64,64 @@ class CaptureCore:
         frame.as_array.tofile(path)
         #Remove frame from memory
         del frame
-
+    
+    def continuous_capture(self, t=1):
+        bu = (ct.c_ubyte*1024000) * 100 
+        self.buffer = bu()
+        frame = self.camera.capture(encoding="raw")
+        frame.buffer_ptr[0].data = self.buffer[0]
+        t_overall = time.time()
+        t_start = time.time()
+        t_end = t_start + t
+        i = 0
+        capture_time = []
+        save_time = []
+        try:
+            while (time.time()<t_end):
+                if i==100: 
+                    i = 0
+                    capture_time = np.array(capture_time)
+                    print("FPS: {:3.1f}, {:3d}".format(100/(time.time()-t_overall),
+                                                       len(capture_time[capture_time>0.011])))
+                    capture_time = []
+                    t_overall = time.time()
+                else: 
+                    #print("Capture {:2d}: ".format(i), end="")
+                    t_start=time.time()
+                    frame = self.camera.capture(encoding="raw")
+                    capture_time.append(time.time()-t_start)
+                    #print("%.5fs"%(time.time()-t_start), end=" ")
+                    t_start = time.time()
+                    self.buffer[i] = ct.cast(frame.buffer_ptr[0].data, ct.POINTER(ct.c_ubyte*1024000)).contents
+                    save_time.append(time.time()-t_start)
+                    #print("%.5fs"%(time.time()-t_start))
+                    del frame
+                    i+=1
+        except KeyboardInterrupt:
+                print('Interrupted')
+        
+        for j in range(self.config["frames_after"]):
+            i+=1
+            if(i==100):
+                i=0
+            frame = self.camera.capture(encoding="raw")
+            self.buffer[i] = ct.cast(frame.buffer_ptr[0].data, ct.POINTER(ct.c_ubyte*1024000)).contents
+        
+        print("Remaining frames taken.")
+        t = time.time()
+        self.buffer = np.roll(self.buffer, -(i+1), axis=0)
+        
+        self.camera.close_camera()
+        print("Camera closed. Saving images . . .")
+        
+        for i in range(self.config["max_frames"]//10):
+            im = Image.fromarray(np.reshape(self.buffer[i],
+                                            tuple(np.array(self.config["resolution"][::-1]))))
+            im = im.convert("L")
+            im.save(self.config["save_path"]+"Buffer-"+"{:02}".format(i)+self.config["image_format"])
+        
+        print("Images saved")
+        
 if __name__ == "__main__":
     c = CaptureCore()
+    c.continuous_capture(t=120)
